c290f0c07fea770976c1f1f8f8df3d77780ca0fa
Henry <hahester@hotmail.com>
2018-10-03 13:23:19 -0400

    edit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,34 +6,16 @@ module.exports = function(app) {
  
 
   app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    res.json();
   });
 
   
+
   
 
-  app.post("/api/friends", function(req, res) {
-    var bestMatch = {
-        name: "",
-        photo: "",
-        friendDifference: 1000
-
-    }
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+  
 
  
-  app.post("/api/clear", function(req, res) {
-    tableData.length = [];
-    waitListData.length = [];
+ 
 
-    res.json({ ok: true });
-  });
 };
